d9e96d2a88c24d4da2d3a3e2cc49225ffc01f02f
Andrew <expandrew@gmail.com>
2017-06-30 16:34:53 -0700

    continue refactoring in order to try and get our doctor information list to display.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,14 +1,15 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-var displayDoctors = function(medicalIssue) {
-  $('.showDoctors').text('Here is a list of doctors in your area that specialize in your medical issue: ' + response);
+var displayDoctors = function(doctors) {
+  $('.showDoctors').text('Here is a list of doctors in your area that specialize in ' + medicalIssue + ':' + doctors);
 };
 
 $(document).ready(function() {
   var currentDoctorObject = new Doctor();
   $('#find').click(function() {
+    event.preventDefault();
     var medicalIssue = $('#medical-issue').val();
-    $('#medical-issue').val();
+    $('#medical-issue').val("");
     // var location = $('#location').val();
     // $('#location').val("");
     currentDoctorObject.getDoctors(displayDoctors);
--- js/doctor.js
+++ js/doctor.js
@@ -1,15 +1,33 @@
 var apiKey = require('./../.env').apiKey;
 
-Doctor = function(){
+Doctor = function(firstName, lastName, title, specialty1, specialty2){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.title = title;
+  this.specialty1 = specialty1;
+  this.specialty2 = specialty2;
 };
 
 Doctor.prototype.getDoctors = function(medicalIssue) {
   console.log(medicalIssue);
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(response) {
+      var doctors = [];
+      objects.forEach(function(object) {
+        object = new Doctor(
+        var firstName = object.profile.first_name;
+        var lastName = object.profile.last_name;
+        var title = object.profile.title;
+        var specialty1 = object.specialties[0];
+        var specialty2 = object.specialties[1];
+      );
+        doctors.push(doctor);
+      });
+      displayDoctors(doctors);
       console.log(response);
-    })
+    });
    .fail(function(error){
+     $('.showDoctors').text(error.responseJSON.message);
       console.log("fail");
     });
 };
